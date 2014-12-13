c429fb5747f6a36bd122d6fc66f6c98eaf229b2c
John Bowden <john@pennywell.ca>
2014-12-13 13:59:45 -0330

    Removed hardcoded ip address. Added function to derive it.

--- service/www_cfg.py
+++ service/www_cfg.py
@@ -1,4 +1,13 @@
-WEB_HOST = '192.168.0.12'
+import socket, fcntl, struct
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+WEB_HOST = get_ip_address('eth0')
 WEB_HTTP_PORT = 80
 
 WEB_LISTEN_HOST = 'localhost'
