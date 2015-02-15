3e17d125014e89b3d19312ae4a9ff52014486292
John Bowden <john@pennywell.ca>
2015-02-15 10:30:39 -0330

    New StreamManager class.

--- radio/rmpd.py
+++ radio/rmpd.py
@@ -44,3 +44,73 @@ class RadioMPDClient(mpd.MPDClient):
 
 # End of class RadioMPDClient
 
+class StreamManager():
+	streams = []
+
+	active_stream = 0
+
+	def __init__(self):
+		pass
+
+	def add_stream(self, host, port):
+		stream = RadioMPDClient(host, port)
+		self.streams.append( stream )
+
+	def del_stream(self, stream_id):
+		try:
+			if stream_id == self.active_stream:
+				self.switch_stream()
+			stream = self.streams[stream_id]
+			stream.close()
+		except KeyError:
+			return False
+
+	def switch_stream(self, stream_id):
+		old_act_id = self.active_stream
+
+		if (stream_id > len(self.streams) - 1) or \
+			(stream_id < 0) or \
+			(stream_id == self.active_stream):
+			return
+
+		self.streams[self.active_stream].disableoutput(0)
+		self.streams[stream_id].enableoutput(0)
+		self.streams[stream_id].play()
+		self.active_stream = stream_id
+
+
+	def next_stream(self):
+		new_id = self.active_stream + 1
+		if new_id > len(self.streams) - 1:
+			new_id = 0
+		if new_id == self.active_stream:
+			return
+		self.switch_stream(new_id)
+
+
+	def prev_stream(self):
+		new_id = self.active_stream - 1
+		if new_id < 0:
+			new_id = len(self.streams) - 1
+		if new_id == self.active_stream:
+			return
+		self.switch_stream(new_id)
+
+
+	def load_playlist(self, stream_id, playlist, shuffle = False):
+		try:
+			stream = self.streams[stream_id]
+			stream.ready()
+			stream.clear()
+			stream.load(playlist)
+			if shuffle:
+				stream.shuffle()
+			stream.disableoutput(0)
+			stream.play()
+			
+		except KeyError:
+			return False
+		except mpd.CommandError:
+			return False
+
+# End of class StreamManager
